84e9d08c683b745db5b010852fd317f1f54954c9
Cosme Diego Da Silva Augusto <cosme.augusto@cjf.jus.br>
2023-05-10 17:24:42 -0300

    anotações JPA para classes de domínio

--- src/main/java/com/cursoAngulaSpring/helpdesk/domain/Tecnico.java
+++ src/main/java/com/cursoAngulaSpring/helpdesk/domain/Tecnico.java
@@ -1,5 +1,36 @@
 package com.cursoAngulaSpring.helpdesk.domain;
 
-public class Tecnico extends Pessoa{
+import java.util.ArrayList;
+import java.util.List;
 
+import com.cursoAngulaSpring.helpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
 }
