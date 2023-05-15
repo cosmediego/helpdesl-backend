c1042a203b91a22838d174b894212d2ff4e062a8
Cosme Diego Da Silva Augusto <cosme.augusto@cjf.jus.br>
2023-05-15 17:42:52 -0300

    Padrão DTO para Técnico

--- src/main/java/com/cursoAngulaSpring/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/cursoAngulaSpring/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoAngulaSpring.helpdesk.domain.Tecnico;
+import com.cursoAngulaSpring.helpdesk.dtos.TecnicoDTO;
 import com.cursoAngulaSpring.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico tecnico = service.findById(id);
-		return ResponseEntity.ok(tecnico);
+		return ResponseEntity.ok(new TecnicoDTO(tecnico));
 	}
 }
