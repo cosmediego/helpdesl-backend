c15e4b2eacf2dba68dfd038b2e25ac1cd37a49af
Cosme Diego Da Silva Augusto <cosme.augusto@cjf.jus.br>
2023-05-16 11:38:06 -0300

    endpoint findall para técnico

--- src/main/java/com/cursoAngulaSpring/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/cursoAngulaSpring/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.cursoAngulaSpring.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cursoAngulaSpring.helpdesk.domain.Tecnico;
 import com.cursoAngulaSpring.helpdesk.dtos.TecnicoDTO;
 import com.cursoAngulaSpring.helpdesk.services.TecnicoService;
+
 
 @RestController
 @RequestMapping(value="/tecnicos")
@@ -22,5 +26,12 @@ public class TecnicoResource {
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico tecnico = service.findById(id);
 		return ResponseEntity.ok(new TecnicoDTO(tecnico));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDTO);
 	}
 }
--- src/main/java/com/cursoAngulaSpring/helpdesk/services/DBService.java
+++ src/main/java/com/cursoAngulaSpring/helpdesk/services/DBService.java
@@ -30,11 +30,16 @@ public class DBService {
 		Tecnico tec1 = new Tecnico(null, "Cosme Diego", "00000000000", "cosme@gmail.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
 		
+		Tecnico tec2 = new Tecnico(null, "João Paulo", "58367373120", "jp@gmail.com", "123");
+		tec2.addPerfil(Perfil.ADMIN);
+		
 		Cliente cli1 = new Cliente(null, "Linus Torval", "81415473480", "torval@email.com", "123");
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado_01", "Primeiro chamado", tec1, cli1);
 		
-		tecnicoReposiroty.saveAll(Arrays.asList(tec1));
+		
+	
+		tecnicoReposiroty.saveAll(Arrays.asList(tec1, tec2));
 		clienteReposiroty.saveAll(Arrays.asList(cli1));
 		chamadoReposiroty.saveAll(Arrays.asList(c1));
 	}
